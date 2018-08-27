7ef84979ed32ffe4ef9b3b63bc3c65c63a74b5e3
Ian Emmons <iemmons@bbn.com>
2018-08-27 09:49:54 -0400

    Forgot to add this file to a previous commit

---
+++ jena/JosekiParliamentClient/src/com/bbn/parliament/jena/joseki/client/CmdLineException.java
@@ -0,0 +1,24 @@
+package com.bbn.parliament.jena.joseki.client;
+
+public class CmdLineException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public CmdLineException() {
+	}
+
+	public CmdLineException(String message) {
+		super(message);
+	}
+
+	public CmdLineException(String format, Object... args) {
+		super(String.format(format, args));
+	}
+
+	public CmdLineException(Throwable cause, String message) {
+		super(message, cause);
+	}
+
+	public CmdLineException(Throwable cause, String format, Object... args) {
+		super(String.format(format, args), cause);
+	}
+}
