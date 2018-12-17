ca28c400a3429331e03ed7ca61f62d8737bcd920
Joseph A. Craig <jcraig@bbn.com>
2018-12-17 14:28:39 -0500

    fixed some bash syntax errors and a couple logical errors

--- jena/JosekiExtensions/resources/conf/unix/InstallService.sh
+++ jena/JosekiExtensions/resources/conf/unix/InstallService.sh
@@ -9,28 +9,29 @@ directory=
 
 while [ "$1" != "" ]; do
 	case $1 in
-		-u | --username )	shift
-							username=$1
-							;;
-		-d | --d )			shift
-							directory=$1
-							;;
-		-h | --help )		echo "$usage"> &2
-							exit
-							;;
-		* )					echo "$usage" >&2
-							exit 1
-							;;
+		-u|--username)	shift
+						username=$1
+						;;
+		-d|--d)			shift
+						directory=$1
+						;;
+		-h|--help)		echo "$usage" >&2
+						exit
+						;;
+		*)				echo "$usage" >&2
+						exit 1
+						;;
 	esac
 	shift
 done
 
-if [ username == "" -o directory == "" ] ; then
+if [ -z "$username" -o -z "$directory" ] ; then
 	echo "$usage" >&2
 	exit 1
 fi
 
-sed -i -e "s/USERNAME/$username/g" $directory/parliament.service
-cp parliament.service /etc/systemd/system/parliament.service
+cp -f parliament.service /etc/systemd/system/parliament.service
+sed -i -e "s/USERNAME/$username/g" /etc/systemd/system/parliament.service
+sed -i -e "s|DIRECTORY|$directory|g" /etc/systemd/system/parliament.service
 systemctl enable parliament
 systemctl start parliament
