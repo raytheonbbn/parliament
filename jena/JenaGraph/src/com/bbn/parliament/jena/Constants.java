d55bed0c4583bf9af54193d735c3fd3eb1111434
Ian Emmons <iemmons@bbn.com>
2018-12-19 09:12:24 -0500

    Fixed whitespace

--- jena/JenaGraph/src/com/bbn/parliament/jena/Constants.java
+++ jena/JenaGraph/src/com/bbn/parliament/jena/Constants.java
@@ -2,29 +2,19 @@ package com.bbn.parliament.jena;
 
 import com.hp.hpl.jena.sparql.util.Symbol;
 
-
 public class Constants {
-   public static final String NAMESPACE = "http://parliament.semwebcentral.org/";
+	public static final String NAMESPACE = "http://parliament.semwebcentral.org/";
+	public static final String PFUNCTION_NAMESPACE = NAMESPACE + "pfunction#";
+	public static final String SYMBOL_PREFIX = NAMESPACE + "symbol#";
 
-   public static final String PFUNCTION_NAMESPACE = NAMESPACE + "pfunction#";
+	/** Symbol for storing a cancel query flag in a query execution context. */
+	public static final Symbol CANCEL_QUERY_FLAG_SYMBOL = createSymbol("cancelled_query");
+	public static final Symbol TREE_WIDTH_OPTIMIZATION = createSymbol("tree_width_optimization");
+	public static final Symbol DYNAMIC_OPTIMIZATION = createSymbol("dynamic_optimization");
+	public static final Symbol DEFAULT_OPTIMIZATION = createSymbol("default_optimization");
+	public static final Symbol UPDATED_STATIC_OPTIMIZATION = createSymbol("updated_static_optimization");
 
-   public static final String SYMBOL_PREFIX = NAMESPACE + "symbol#";
-
-   public static Symbol createSymbol(String name) {
-      return Symbol.create(SYMBOL_PREFIX + name);
-   }
-
-   /**
-    * Symbol for storing a cancel query flag in a query execution context.
-    */
-   public static final Symbol CANCEL_QUERY_FLAG_SYMBOL = createSymbol("cancelled_query");
-
-   public static final Symbol TREE_WIDTH_OPTIMIZATION = createSymbol("tree_width_optimization");
-
-   public static final Symbol DYNAMIC_OPTIMIZATION = createSymbol("dynamic_optimization");
-
-   public static final Symbol DEFAULT_OPTIMIZATION = createSymbol("default_optimization");
-
-   public static final Symbol UPDATED_STATIC_OPTIMIZATION = createSymbol("updated_static_optimization");
-
+	private static Symbol createSymbol(String name) {
+		return Symbol.create(SYMBOL_PREFIX + name);
+	}
 }
--- jena/JenaGraph/src/com/bbn/parliament/jena/Kb.java
+++ jena/JenaGraph/src/com/bbn/parliament/jena/Kb.java
@@ -1,5 +1,7 @@
 package com.bbn.parliament.jena;
 
+import com.bbn.parliament.jena.query.KbOpExecutor;
+import com.bbn.parliament.jena.query.KbQueryEngine;
 import com.bbn.parliament.jena.query.KbStageGenerator;
 import com.bbn.parliament.jena.query.index.pfunction.EnableIndexing;
 import com.bbn.parliament.jena.query.optimize.KbOptimize;
@@ -10,44 +12,41 @@ import com.hp.hpl.jena.sparql.engine.main.StageGenerator;
 import com.hp.hpl.jena.sparql.pfunction.PropertyFunctionRegistry;
 
 public class Kb {
+	private static boolean initialized = false;
 
-   private static boolean initialized = false;
+	static {
+		doInitialization();
+	}
 
-   static {
-      doInitialization();
-   }
+	private static synchronized void doInitialization() {
+		// Called at start.
+		if (initialized) {
+			return;
+		}
+		initialized = true;
+		ARQ.init();
+		// insert our op executor into the SPARQL engine
+		QC.setFactory(ARQ.getContext(), KbOpExecutor.KbOpExecutorFactory);
+		StageGenerator orig = (StageGenerator) ARQ.getContext().get(ARQ.stageGenerator);
+		StageGenerator generator = new KbStageGenerator(orig);
+		StageBuilder.setGenerator(ARQ.getContext(), generator);
 
-   private static synchronized void doInitialization() {
-      // Called at start.
-      if (initialized)
-         return;
-      initialized = true;
-      ARQ.init();
-      // insert our op executor into the SPARQL engine
-      QC.setFactory(ARQ.getContext(),
-                    com.bbn.parliament.jena.query.KbOpExecutor.KbOpExecutorFactory);
-      StageGenerator orig = (StageGenerator) ARQ.getContext()
-            .get(ARQ.stageGenerator);
-      StageGenerator generator = new KbStageGenerator(orig);
-      StageBuilder.setGenerator(ARQ.getContext(), generator);
+		KbQueryEngine.register();
+		KbOptimize.register();
 
-      com.bbn.parliament.jena.query.KbQueryEngine.register();
-      KbOptimize.register();
+		registerPropertyFunctions();
+	}
 
-      registerPropertyFunctions();
-   }
+	private static void registerPropertyFunctions() {
+		PropertyFunctionRegistry.get().put(EnableIndexing.URI, EnableIndexing.class);
+	}
 
-   private static synchronized void registerPropertyFunctions() {
-      PropertyFunctionRegistry.get().put(EnableIndexing.URI,
-                                         EnableIndexing.class);
-   }
-
-   /**
-    * Kb System initialization - normally, this is not explicitly called because
-    * all routes to use Parliament will cause initialization to occur. However,
-    * calling it repeatedly is safe and low cost.
-    */
-   public static void init() {
-      // don't do anything here, just forces initialization to occur.
-   }
+	/**
+	 * Kb System initialization - normally, this is not explicitly called because
+	 * all routes to use Parliament will cause initialization to occur. However,
+	 * calling it repeatedly is safe and low cost.
+	 */
+	public static void init() {
+		// don't do anything here, just forces initialization to occur.
+	}
 }
