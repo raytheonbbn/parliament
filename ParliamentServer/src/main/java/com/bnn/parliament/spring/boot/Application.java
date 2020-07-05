7bc156f00668525954f5d37c3640eee757dfd41f
Ian Emmons <iemmons@raytheon.com>
2020-07-05 18:09:41 -0400

    Minor fixups

--- ParliamentServer/src/main/java/com/bnn/parliament/spring/boot/Application.java
+++ ParliamentServer/src/main/java/com/bnn/parliament/spring/boot/Application.java
@@ -2,17 +2,18 @@ package com.bnn.parliament.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * Spring Boot Server for Parliament
- * 
- * @author pwilliams
  *
+ * @author pwilliams
  */
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		try (ConfigurableApplicationContext context = SpringApplication.run(Application.class, args)) {
+			// Do nothing
+		}
 	}
 }
--- ParliamentServer/src/main/java/com/bnn/parliament/spring/boot/Controller.java
+++ ParliamentServer/src/main/java/com/bnn/parliament/spring/boot/Controller.java
@@ -1,37 +1,38 @@
 package com.bnn.parliament.spring.boot;
 
-
 import java.util.List;
-import java.util.Map;
 
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Controller for Spring Boot Server. Routes HTTP requests from /parliament/sparql to appropriate request method.
- * 
- * @author pwilliams
  *
+ * @author pwilliams
  */
 @RestController
 public class Controller {
-	
 	private static final String ENDPOINT = "/parliament/sparql";
 	private static final String URL_ENCODED = "application/x-www-form-urlencoded";
 	private static final String SPARQL_QUERY = "application/sparql-query";
 	private static final String SPARQL_UPDATE = "application/sparql-update";
 	private static final String DEFAULT_GRAPH = "Placeholder Graph URI";
 
-	
 	@GetMapping(value = ENDPOINT, params = "query")
 	public String sparqlGET(
-			@RequestParam(value = "query") String query, 
+			@RequestParam(value = "query") String query,
 			@RequestParam(value = "default-graph-uri", defaultValue = "") List<String> defaultGraphURI,
 			@RequestParam(value = "named-graph-uri", defaultValue = "") List<String> namedGraphURI) {
-		
+
 		return String.format("GET Success! Testing changes Query: %1s, %2s", query, defaultGraphURI.toString());
 	}
-	
+
 	// Spring does not allow the use of @RequestBody when using URL_ENCODED, so we must use @RequestParam
 	@PostMapping(value = ENDPOINT, consumes = URL_ENCODED, params = "query")
 	public String sparqlURLEncodeQueryPOST(
@@ -39,95 +40,88 @@ public class Controller {
 			@RequestParam(value = "default-graph-uri", defaultValue = "") List<String> defaultGraphURI,
 			@RequestParam(value = "named-graph-uri", defaultValue = "") List<String> namedGraphURI
 			) {
-		
+
 		return String.format("POST Success! Query: %s", query);
 	}
-	
+
 	@PostMapping(value = ENDPOINT, consumes = SPARQL_QUERY)
 	public String sparqlDirectQueryPOST(
 			@RequestParam(value = "default-graph-uri", defaultValue = "") List<String> defaultGraphURI,
 			@RequestParam(value = "named-graph-uri", defaultValue = "") List<String> namedGraphURI,
 			@RequestBody String query) {
-		
+
 		return String.format("POST Success! Query: %s", query);
 	}
-	
+
 	//Update equivalents of above (next 2 requests)
-	
+
 	@PostMapping(value = ENDPOINT, consumes = URL_ENCODED, params = "update")
 	public String sparqlURLEncodeUpdatePOST(
 			@RequestParam(value = "update") String update,
 			@RequestParam(value = "using-graph-uri", defaultValue = "") List<String> defaultGraphURI,
 			@RequestParam(value = "using-named-graph-uri", defaultValue = "") List<String> namedGraphURI) {
-		
+
 		return String.format("POST Success! Update: %s", update);
 	}
-	
-	
+
+
 	@PostMapping(value = ENDPOINT, consumes = SPARQL_UPDATE)
 	public String sparqlDirectUpdatePOST(
 			@RequestParam(value = "using-graph-uri", defaultValue = "") List<String> defaultGraphURI,
 			@RequestParam(value = "using-named-graph-uri", defaultValue = "") List<String> namedGraphURI,
 			@RequestBody String update) {
-		
+
 		return String.format("POST Success! Update: %s", update);
 	}
-	
+
 	//HEAD mapping automatically supported by GET mapping
 	@GetMapping(value = ENDPOINT, params = "graph")
 	public String sparqlGraphGET(@RequestParam(value = "graph") String graphURI) {
-		
 		return String.format("placeholder");
 	}
-	
+
 	@GetMapping(value = ENDPOINT, params = "default")
 	public String sparqlGraphDefaultGET(@RequestParam(value = "default") String defaultGraph) {
 		return sparqlGraphGET(DEFAULT_GRAPH);
 	}
-	
+
 	@PutMapping(value = ENDPOINT, params = "graph")
 	public String sparqlGraphPUT(@RequestParam(value = "graph") String graphURI, @RequestBody String payload) {
-		
 		return String.format("The payload is: %s", payload);
 	}
-	
+
 	@PutMapping(value = ENDPOINT, params = "default")
 	public String sparqlGraphDefaultPUT(@RequestParam(value = "default") String defaultGraph, @RequestBody String payload) {
 		return sparqlGraphPUT(DEFAULT_GRAPH, payload);
 	}
-	
+
 	@DeleteMapping(value = ENDPOINT, params = "graph")
 	public String sparqlGraphDELETE() {
-		
 		return String.format("placeholder");
 	}
-	
+
 	@DeleteMapping(value = ENDPOINT, params = "default")
 	public String sparqlGraphDefaultDELETE() {
-		
 		return String.format("placeholder");
 	}
-	
+
 	@PostMapping(value = ENDPOINT, params = "graph")
 	public String sparqlGraphPOST(@RequestParam(value = "graph") String graphURI, @RequestBody String payload) {
-		
 		return String.format("The payload is: %s", payload);
 	}
-	
+
 	@PostMapping(value = ENDPOINT, params = "default")
 	public String sparqlGraphDefaultPOST(@RequestParam(value = "default") String defaultGraph, @RequestBody String payload) {
 		return sparqlGraphPOST(DEFAULT_GRAPH, payload);
 	}
-	
+
 	@PatchMapping(value = ENDPOINT, params = "graph")
 	public String sparqlGraphPATCH(@RequestParam(value = "graph") String graphURI, @RequestBody String payload) {
-		
 		return String.format("The payload is: %s", payload);
 	}
-	
+
 	@PatchMapping(value = ENDPOINT, params = "default")
 	public String sparqlGraphDefaultPATCH(@RequestParam(value = "default") String defaultGraph, @RequestBody String payload) {
 		return sparqlGraphPATCH(DEFAULT_GRAPH, payload);
 	}
-
 }
