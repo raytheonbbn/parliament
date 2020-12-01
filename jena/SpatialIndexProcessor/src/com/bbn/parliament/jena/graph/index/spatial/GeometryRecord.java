47120e047a87b94409da708f27278378c10de141
Ian Emmons <iemmons@raytheon.com>
2020-12-01 17:43:29 -0500

    Minor fixes prior to merging

--- jena/SpatialIndexProcessor/src/com/bbn/parliament/jena/graph/index/spatial/GeometryRecord.java
+++ jena/SpatialIndexProcessor/src/com/bbn/parliament/jena/graph/index/spatial/GeometryRecord.java
@@ -4,27 +4,25 @@ import com.bbn.parliament.jena.graph.index.Record;
 import com.hp.hpl.jena.graph.Node;
 import com.vividsolutions.jts.geom.Geometry;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 /**
  * This class exists as something of a hack work-around for a shortcoming of the
  * JTS Geometry class. Normally, this class would not exist and we would simply
- * use Record&lt;Geometry&gt; instead. However, the JTS Geometry class does not
- * properly implement hashCode() and equals() methods. The documentation for
- * that class says this:
+ * use {@code Record<Geometry>} instead. However, the JTS Geometry class does
+ * not properly implement {@code hashCode()} and {@code equals()} methods. The
+ * documentation for that class says this:
  *
  * <blockquote>#equals and #hashCode are not overridden, so that when two
  * topologically equal Geometries are added to HashMaps and HashSets, they
  * remain distinct. This behavior is desired in many cases.</blockquote>
  *
  * Why this is so is a bit of a mystery to me, but unfortunately we sometimes
- * need to test equality of these things. (See for example the testIterator
- * method in class com.bbn.parliament.jena.query.index.IndexTestMethods.) As a
- * partial work-around, we derive this class, override equals, and use the
- * compareTo method on Geometry to test for equality. I say partial, because
- * this does not help implement hashCode. So, I've disabled hashCode by throwing
- * NotImplementedException from it. This means that GeometryRecord instances can
- * never be stored in a HashMap (as keys) or in a HashSet.
+ * need to test equality of these things. (See for example the {@code testIterator}
+ * method in class {@code com.bbn.parliament.jena.query.index.IndexTestMethods}.)
+ * As a partial work-around, we derive this class, override {@code equals}, and use the
+ * {@code compareTo} method on Geometry to test for equality. I say partial, because
+ * this does not help implement {@code hashCode}. So, I've disabled {@code hashCode} by throwing
+ * {@code UnsupportedOperationException} from it. This means that {@code GeometryRecord} instances can
+ * never be stored in a {@code HashMap} (as keys) or in a {@code HashSet}.
  *
  * @author rbattle
  */
@@ -53,7 +51,7 @@ public class GeometryRecord extends Record<Geometry> {
 	/** {@inheritDoc} */
 	@Override
 	public int hashCode() {
-		throw new NotImplementedException();
+		throw new UnsupportedOperationException();
 	}
 
 	/** {@inheritDoc} */
